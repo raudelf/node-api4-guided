dc9ac1a45f87d6a64a1b7880b75d22dde0b11ac0
raudelf <rawdell@outlook.com>
2020-03-18 21:37:27 -0500

    Testing heroku

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI / CD Pipeline Stages:
+// 1. Development (dev)
+// 2. Test
+// 3. Staging
+// *  --- Manual Gate
+// 4. Production
